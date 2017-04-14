6e1e0668a93a83cc67b05b0e33cff3d14d9e56c6
Alex Wilmer <djazium@gmail.com>
2017-04-14 17:55:03 -0400

    ✨ react-router

--- front/ui/src/App.js
+++ front/ui/src/App.js
@@ -1,4 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-let App = () => <div>hey</div>
+let App = () =>
+  <Router>
+    <div>
+      <h1>Venn</h1>
+      <Link to="/">Home</Link>
+      <Link to="/test">Test</Link>
+      <div>
+        <Route path="/test" component={() => <div>test</div>} />
+      </div>
+    </div>
+  </Router>
+
 export default App
